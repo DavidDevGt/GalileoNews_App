dd2993dfc255c2f1b544296bc35d984c38662490
DavidDevGt <josuedavidvl18@gmail.com>
2024-12-01 00:54:36 -0600

    Update API_URL to point to the production server

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3001";
+const API_URL = import.meta.env.VITE_BACKEND_URL;
 
 export default {
   async login(email: string, password: string) {
